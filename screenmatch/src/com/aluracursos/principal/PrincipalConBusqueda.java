fd232dd3c99f857700aa07c7b5197f2da10b9199
ntmisel <ntmisel@gmail.com>
2024-06-21 10:01:15 -0600

    API constrution

---
+++ screenmatch/src/com/aluracursos/principal/PrincipalConBusqueda.java
@@ -0,0 +1,29 @@
+package com.aluracursos.principal;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalConBusqueda {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escribe nombre de la pelicula:");
+        var busqueda = scanner.nextLine();
+
+        String direccion = "https://www.omdbapi.com/?t="+busqueda+"&apikey=f3eb0070";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(direccion))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+
+    }
+}
