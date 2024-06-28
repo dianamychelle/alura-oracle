876018b56214d6db823614bfe46398a7be5e4f9a
ntmisel <ntmisel@gmail.com>
2024-06-27 22:50:13 -0600

    Grabando un texto en un archivo

--- screenmatch/src/com/aluracursos/principal/PrincipalConBusqueda.java
+++ screenmatch/src/com/aluracursos/principal/PrincipalConBusqueda.java
@@ -6,6 +6,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -46,6 +47,11 @@ public class PrincipalConBusqueda {
 
                 Titulo miTitulo = new Titulo(miTituloOmdb);
                 System.out.println("Titulo convertido: " + miTitulo);
+
+            FileWriter escritura = new FileWriter("peliculas.txt");
+            escritura.write(miTitulo.toString());
+            escritura.close();
+
         }catch(NumberFormatException e){
                 System.out.println("Ocurrió un error");
                 System.out.println(e.getMessage());
