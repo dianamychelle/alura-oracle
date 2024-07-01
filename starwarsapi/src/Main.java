f2fcb046762ed8f24115f7cad004054f4ceb614e
ntmisel <ntmisel@gmail.com>
2024-07-01 16:56:26 -0600

    Input by user of number of movie

--- starwarsapi/src/Main.java
+++ starwarsapi/src/Main.java
@@ -1,23 +1,25 @@
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         ConsultaPelicula consulta = new ConsultaPelicula();
+        System.out.println("Numero:");
+
         try{
-            Pelicula pelicula = consulta.buscaPelicula(1);
+            var numeroDePelicula = Integer.valueOf(sc.nextLine());
+            Pelicula pelicula = consulta.buscaPelicula(numeroDePelicula);
             System.out.println(pelicula);
+        }catch (NumberFormatException e){
+            System.out.println("Numero no encontrado"+e.getMessage());
         }catch (RuntimeException e){
             System.out.println(e.getMessage());
             System.out.println("Finalizando aplicación.");
         }
-
-
-
-
-
-
 
         }
     }
