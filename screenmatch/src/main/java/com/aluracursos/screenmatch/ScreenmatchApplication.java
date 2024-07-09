0bae50e724b18a6f5f1f4c0907156dc8368aea29
ntmisel <ntmisel@gmail.com>
2024-07-08 21:56:47 -0600

    Consuming our API

--- screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.service.ConsumoAPI;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
+		//System.out.println("Este es mi primer Adios mundo en Spring.Bienvenido. ");
+		var consumoApi = new ConsumoAPI();
+		var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Game+of+Thrones&apikey=f3eb0070");
+
+		System.out.println(json);
 
 	}
 
