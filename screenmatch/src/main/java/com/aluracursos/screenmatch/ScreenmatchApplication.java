df510e0a053f9bfa396919475d095eea190bb1bb
ntmisel <ntmisel@gmail.com>
2024-07-10 23:37:25 -0600

    Mapping data only about episode

--- screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
+import com.aluracursos.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,8 +14,12 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		//System.out.println("Este es mi primer Adios mundo en Spring.Bienvenido. ");
 		var consumoApi = new ConsumoAPI();
 		var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Game+of+Thrones&apikey=f3eb0070");
-		System.out.println(json);
+		System.out.println("Json de la serie:"+json);
 
+		ConvierteDatos conversor = new ConvierteDatos();
+		var datos = conversor.obtenerDatos(json, DatosSerie.class);
+		System.out.println("Solo requerimos estos "+datos);
+		//json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Game+of+Thrones&Season=1&episode=1&apikey=f3eb0070");
 	}
 
 	public static void main(String[] args) {
